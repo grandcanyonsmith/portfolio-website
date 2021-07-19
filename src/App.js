f17777523657d8c152f8dea1a8af7cda7e59ba0e
Canyon Smith <canyonfsmith@gmail.com>
2021-07-19 02:25:34 -0600

    Made it only desktop functionality. Still need to get it mobile responsive

--- src/App.js
+++ src/App.js
@@ -20,21 +20,56 @@ function App() {
         </a>
       </header> */}
       <div className="image-container">
-      <div>
+      <div className="row-1">
+      <div className="text-container">
+        <h1> Valentwear site.</h1>
+      </div>
+      <div className="image-box">
       <a href="https://valentwear.com/" id="bottle" onclick="document.location=this.id+'.html';return false;" >
       <Image src="https://res.cloudinary.com/apexx/image/upload/v1626656464/IMG_2768_dgr2ak.jpg" rounded  className="image-right"/>
       </a>
       </div>
-      <div>
-      <a href="https://mariobasner.com/" id="bottle" onclick="document.location=this.id+'.html';return false;" >
+      </div>
+      <div className="row-1">
+      <div className="image-box">
+      <a href="https://mariobasner.com//" id="bottle" onclick="document.location=this.id+'.html';return false;" >
       <Image src="https://res.cloudinary.com/apexx/image/upload/v1626660919/MarioB_Website_Mock_p3kfhm.jpg" rounded  className="image-left"/>
       </a>
-    </div>
-    <div>
-      <a href="https://siliconstonks.com/performance/11/2020" id="bottl e" onclick="document.location=this.id+'.html';return false;" >
-      <Image src="https://res.cloudinary.com/apexx/image/upload/v1626658801/IMG_2769_xqjeso.jpg" rounded  className="image-right-ll"/>
+
+      </div>
+      <div className="text-container">
+        <h1>Mario Basner Art Museum</h1>
+        
+      </div>
+      </div>
+      <div className="row-1">
+      <div className="text-container">
+      <h1>Mosaic</h1>
+        <p>
+        
+        </p>
+      </div>
+      <div stlye="margin-left: 10em; margin-top: 5em; padding-top:100px;"className="image-box">
+      <a href="https://mosaic-new.herokuapp.com/outfit/622" id="bottle" onclick="document.location=this.id+'.html';return false;" >
+      <Image src="https://res.cloudinary.com/apexx/image/upload/v1626679406/IMG_2774_z0r0kb.jpg" rounded  className="image-left-ll"/>
       </a>
-    </div>
+
+      </div>
+      </div>
+      <div className="row-1">
+      <div className="text-container">
+        <h1>Silicon Stonks</h1>
+        <p>
+        
+        </p>
+      </div>
+      <div className="image-box">
+      <a href="https://siliconstonks.com/latest" id="bottle" onclick="document.location=this.id+'.html';return false;" >
+      <Image src="https://res.cloudinary.com/apexx/image/upload/v1626658801/IMG_2769_xqjeso.jpg" rounded  className="image-left"/>
+      </a>
+
+      </div>
+      </div>
     </div>
     </div>
 

75a6abce764b4c28d593cabd62536b7035f17005
Canyon Smith <canyonfsmith@gmail.com>
2021-07-19 17:59:01 -0600

    asdf
    Okay bet

--- src/App.js
+++ src/App.js
@@ -42,9 +42,9 @@ function App() {
         </div>
         </div>
         <div className="social-media-icons">
-      <a href="https://github.com/grandcanyonsmith/" class="fa fa-github"></a>
-      <a href="https://twitter.com/grandcanyon77?lang=en" class="fa fa-twitter"></a>
-      <a href="https://www.instagram.com/grandcanyonsmith/?hl=en" class="fa fa-instagram"></a>
+      <a class="icon" href="https://github.com/grandcanyonsmith/" class="fa fa-github"></a>
+      <a class="icon" href="https://www.linkedin.com/in/canyon-smith-7a6445174/" class="fa fa-linkedin"></a>
+      <a class="icon" href="https://www.instagram.com/grandcanyonsmith/?hl=en" class="fa fa-instagram"></a>
       {/* <Music></Music> */}
       </div>
       

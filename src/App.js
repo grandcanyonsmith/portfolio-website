e6a46b372dace0bb4a29466fd203827c23ac34d5
Canyon Smith <canyonfsmith@gmail.com>
2021-07-18 20:20:23 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import {Image, Container} from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,8 +18,26 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <div className="image-container">
+      <div>
+      <a href="https://valentwear.com/" id="bottle" onclick="document.location=this.id+'.html';return false;" >
+      <Image src="https://res.cloudinary.com/apexx/image/upload/v1626656464/IMG_2768_dgr2ak.jpg" rounded  className="image-right"/>
+      </a>
+      </div>
+      <div>
+      <a href="https://mariobasner.com/" id="bottle" onclick="document.location=this.id+'.html';return false;" >
+      <Image src="https://res.cloudinary.com/apexx/image/upload/v1626660919/MarioB_Website_Mock_p3kfhm.jpg" rounded  className="image-left"/>
+      </a>
     </div>
+    <div>
+      <a href="https://siliconstonks.com/performance/11/2020" id="bottle" onclick="document.location=this.id+'.html';return false;" >
+      <Image src="https://res.cloudinary.com/apexx/image/upload/v1626658801/IMG_2769_xqjeso.jpg" rounded  className="image-right"/>
+      </a>
+    </div>
+    </div>
+    </div>
+
   );
 }
 
